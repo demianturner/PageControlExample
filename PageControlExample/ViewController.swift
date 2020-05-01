e2d4c49a2a44dd8aa350df73e22b293006fbce30
Demian Turner <demian@muse23.com>
2020-05-01 12:06:20 +0100

    Initial Commit

---
+++ PageControlExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PageControlExample
+//
+//  Created by Demian Turner on 01/05/2020.
+//  Copyright © 2020 Demian Turner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
